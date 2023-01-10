73f4e40e27945b0515c8527a8cb64b090812b63c
Karibasaveshwara TG <karibasaveshwara.tg@moonraft.com>
2023-01-10 12:00:21 +0530

    first commit

---
+++ src/components/ClassBasedComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ClassBasedComponent extends React.Component {
+  render() {
+    return <h1>This is Class Based Component</h1>;
+  }
+}
+
+export default ClassBasedComponent;
---
+++ src/components/FunctionBasedComponent.js
@@ -0,0 +1,7 @@
+import "../App.css";
+
+const FunctionBasedComponent = () => {
+  return <h1 className="item">This is Function Based Component</h1>;
+};
+
+export default FunctionBasedComponent;
